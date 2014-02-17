9846f00f7b10d2a52e8d0f839600a5785257da18
sunyangg <cutegoat20041@hotmail.com>
2014-02-17 22:37:27 +0800

    這是WEEK 1 第九題的檔案

---
+++ week1_9.java
@@ -0,0 +1,10 @@
+public class week1_9
+{
+	public static void main(String args[])
+	{
+		System.out.println("朝辭白帝彩雲間\n");
+		System.out.println("千里江陵一日還\n");
+		System.out.println("兩岸猿聲啼不住\n");
+		System.out.println("兩岸猿聲啼不住\n");
+	}
+}
